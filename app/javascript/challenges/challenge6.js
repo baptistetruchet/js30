2b4e203b8057d917675e013ebd1942f3dc9f847f
Baptiste Truchet <baptiste.truchet@gmail.com>
2018-06-18 14:36:39 +0200

    finish challenge 6

--- app/javascript/challenges/challenge6.js
+++ app/javascript/challenges/challenge6.js
@@ -1,5 +1,50 @@
+function findMatches(wordToMatch, cities) {
+  return cities.filter(place => {
+    const regex = new RegExp(wordToMatch, 'gi');
+    return place.city.match(regex) || place.state.match(regex);
+  });
+}
+
+function numberWithCommas(num) {
+  return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+
 function challenge6() {
-  console.log('hi challenge6')
+  const endpoint = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json';
+
+  const cities = [];
+
+  fetch(endpoint)
+    .then(response => response.json())
+    .then(data => {
+      cities.push(...data);
+    });
+
+  const searchInput = document.querySelector('.search');
+  const suggestions = document.querySelector('.suggestions');
+
+  searchInput.addEventListener('keyup', function() {
+    if (this.value === "") {
+      suggestions.innerHTML = `
+        <li>Filter for a city</li>
+        <li>or a state</li>
+      `;
+    } else {
+      const matchArray = findMatches(this.value, cities);
+      const html = matchArray.map(place => {
+        const regex = new RegExp(this.value, 'gi');
+        const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
+        const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
+        return `
+          <li>
+            <span class="name">${cityName}, ${stateName}</span>
+            <span class="population">${numberWithCommas(place.population)}</span>
+          </li>
+        `;
+      }).join('');
+      suggestions.innerHTML = html;
+    }
+  });
 }
 
 export { challenge6 }
