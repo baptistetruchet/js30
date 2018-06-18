9dd33fdc30ec6aac93367e5631f7744e32e3c747
Baptiste Truchet <baptiste.truchet@gmail.com>
2018-06-18 12:43:13 +0200

    finish challenge 5

--- app/javascript/challenges/challenge5.js
+++ app/javascript/challenges/challenge5.js
@@ -1,5 +1,19 @@
-function challenge5() {
+function toggleOpen() {
+  const open = document.querySelector('.open');
+  if (open && open !== this) { open.classList.toggle('open') }
+  this.classList.toggle('open');
+}
 
+function toggleActive(e) {
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+function challenge5() {
+  const panels = document.querySelectorAll('.panel');
+  panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+  panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
 }
 
 export { challenge5 }
