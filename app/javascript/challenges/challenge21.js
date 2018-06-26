a9ba6f82381acdd538a80b5ba9dc6680042a4e72
Baptiste Truchet <baptiste.truchet@gmail.com>
2018-06-26 13:46:56 +0200

    finish challenge 21

--- app/javascript/challenges/challenge21.js
+++ app/javascript/challenges/challenge21.js
@@ -1,5 +1,14 @@
 function challenge21() {
+  const arrow = document.querySelector('.arrow');
+  const speed = document.querySelector('.speed-value');
 
+  navigator.geolocation.watchPosition((data) => {
+    console.log(data);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  }, (err) => {
+    console.error(err);
+  });
 }
 
 export { challenge21 }
